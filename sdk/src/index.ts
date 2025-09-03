8a8e22d1b2ee83ce458f92f18314304a0bcb6ecc
arjunpotter17 <arjunpotter17@gmail.com>
2025-09-03 09:38:54 +0530

    configure devnet and mainnet options

--- sdk/src/index.ts
+++ sdk/src/index.ts
@@ -1,35 +1,45 @@
-import { Program } from '@coral-xyz/anchor'
-import { Connection, PublicKey } from '@solana/web3.js'
-import { Depredict } from './types/depredict.js'
-import IDL from './types/depredict.json'  // tsup will handle this
-import Trade from './trade.js'
-import Config from './config.js'
-import Position from './position.js'
+import { Program } from "@coral-xyz/anchor";
+import { Connection, PublicKey } from "@solana/web3.js";
+import { Depredict } from "./types/depredict.js";
+import DEVNET_IDL from "./types/depredict-devnet.json"; // tsup will handle this
+import MAINNET_IDL from "./types/depredict-mainnet.json"; // tsup will handle this
+import Trade from "./trade.js";
+import Config from "./config.js";
+import Position from "./position.js";
 
 // Re-export all types
-export * from './types/trade.js'
-export * from './types/position.js'
-export * from './types/index.js'
+export * from "./types/trade.js";
+export * from "./types/position.js";
+export * from "./types/index.js";
 
 // Re-export constants
-export * from './utils/constants.js'
+export * from "./utils/constants.js";
 
 export default class DepredictClient {
-  program: Program<Depredict>
-  trade: Trade
-  config: Config
-  position: Position
-  ADMIN_KEY: PublicKey
-  FEE_VAULT: PublicKey
-  
-  constructor(connection: Connection, adminKey: PublicKey, feeVault: PublicKey) {
-    this.program = new Program(IDL as Depredict, { connection })
-    this.trade = new Trade(this.program, adminKey, feeVault)
-    this.position = new Position(this.program)
-    this.config = new Config(this.program, adminKey, feeVault)
-    this.ADMIN_KEY = adminKey
-    this.FEE_VAULT = feeVault
+  program: Program<Depredict>;
+  trade: Trade;
+  config: Config;
+  position: Position;
+  ADMIN_KEY: PublicKey;
+  FEE_VAULT: PublicKey;
+
+  constructor(
+    connection: Connection,
+    adminKey: PublicKey,
+    feeVault: PublicKey,
+  ) {
+
+    console.log('SDK: this is connection', connection.rpcEndpoint)
+    this.program = new Program(
+      connection.rpcEndpoint.includes("devnet")
+        ? (DEVNET_IDL as Depredict)
+        : (MAINNET_IDL as Depredict),
+      { connection }
+    );
+    this.trade = new Trade(this.program, adminKey, feeVault);
+    this.position = new Position(this.program);
+    this.config = new Config(this.program, adminKey, feeVault);
+    this.ADMIN_KEY = adminKey;
+    this.FEE_VAULT = feeVault;
   }
 }
-
-
